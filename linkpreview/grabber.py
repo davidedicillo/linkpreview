43e17d69b65e1b86d523f4b4ef2b87cfa1a5ecb6
Davide <davide@dicillo.com>
2021-02-14 19:00:43 -0800

    updated the header

--- linkpreview/grabber.py
+++ linkpreview/grabber.py
@@ -29,7 +29,7 @@ class LinkGrabber:
 
     def get_content(self, url: str):
         headers = {
-            'User-Agent': 'My User Agent 1.0',
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36',
             'From': 'youremail@domain.com'  # This is another valid field
         }
         r = requests.get(

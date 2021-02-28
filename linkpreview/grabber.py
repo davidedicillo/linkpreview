d0293e35e1ec76eaf41b86725b65079d0392d946
Davide <davide@dicillo.com>
2021-02-28 22:22:05 +0000

    adding some print for debugging

--- linkpreview/grabber.py
+++ linkpreview/grabber.py
@@ -30,7 +30,7 @@ class LinkGrabber:
     def get_content(self, url: str):
         headers = {
             'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36',
-            'From': 'youremail@domain.com'  # This is another valid field
+            'From': 'info@listlink.co'  # This is another valid field
         }
         r = requests.get(
             url, stream=True, timeout=self.initial_timeout, headers=headers
@@ -39,14 +39,17 @@ class LinkGrabber:
 
         content_type = r.headers.get("content-type")
         if not content_type:
+            print("Invalid content type")
             raise InvalidContentError("Invalid content type")
 
         mime_type = content_type.split(";")[0].lower()
         if mime_type != "text/html":
+            print("Invalid mime type")
             raise InvalidMimeTypeError("Invalid mime type")
 
         length = r.headers.get("Content-Length")
         if length and int(length) > self.maxsize:
+            print("response too large")
             raise MaximumContentSizeError("response too large")
 
         size = 0
@@ -54,10 +57,12 @@ class LinkGrabber:
         content = b""
         for chunk in r.iter_content(self.chunk_size):
             if time.time() - start > self.receive_timeout:
+                print("timeout reached")
                 raise TimeoutError("timeout reached")
 
             size += len(chunk)
             if size > self.maxsize:
+                print("response too large")
                 raise MaximumContentSizeError("response too large")
 
             content += chunk
